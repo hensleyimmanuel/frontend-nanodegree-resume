aab641fc015b84928cb7ffa14b1cdde9cf3040a7
Immanuel Hensley <hensleyimmanuel@yahoo.com>
2015-01-08 22:31:22 -0500

    added name and title to HTML

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,1 +1,9 @@
 $("#main").append("Immanuel Hensley");
+
+var name = "Immanuel Hensley";
+var role = "Front-End Web Developer";
+var formattedName = HTMLheaderName.replace("%data%", name);
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
