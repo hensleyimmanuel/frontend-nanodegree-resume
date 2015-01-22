a10d5459abc405a02319242c95a7aba97beea2ae
Immanuel Hensley <immanuel.hensley@COTSOL5GLX5S1.fin.ds.ky.gov>
2015-01-22 13:26:06 -0500

    Added Skills and Work Descritption

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,5 @@
+// -- Lines below hold all information on my resume --
+
 var bio = {
     "name": "Immanuel Hensley",
     "role": "Front-End Web Developer",
@@ -6,20 +8,13 @@ var bio = {
         "email": "hensleyimmanuel@yahoo.com",
         "location": "500 Shenandoah Dr. Frankfort Kentucky 40601",
         "facebook": "https://www.facebook.com/hensleyimmanuel",
-		"github": "hensleyimmanuel"
-		
-    },
+        "github": "hensleyimmanuel"
+        },
     "welcomeMessage": "Hello welecome to my Resume please feel free to contact me.",
-    "skills": [
-        "awesomness",
-        "smart",
-        "programming"
-    ],
+    "skills": ["awesomness","smart","programming"],
     "Pic": "me"
-}
-bio.work = {
-    "jobs": [
-        {
+};
+work = {
     "jobs": [
         {
             "employer": "Pomeroy",
@@ -38,26 +33,10 @@ bio.work = {
             "title": "IT Staff Assistant",
             "date": "06/23/2011 - 08/16/2012",
             "description": "Wrote complex routines for System Procedures and diagnostic routines. Installed and maintained hardware and software such as operating systems, control systems, workstations, peripherals and communications devices, proprietary software packages, information dissemination tools, electronic mail and data base management software. Performed complex system generations, network device configuration generations, workstation installation, network address definitions. Assisted the development, network and production support staff in analyzing requirements for development, modification or retirement of state level or complex departmental systems. I was responsible for system support functions during relocation or conversion of critical data files and equipment. Monitored performance of servers on local area networks and takes action to mitigate network or system degradation. Coordinated the diagnostics and problem resolution with network support staff. Received complex problem reports, and resolves or refers to appropriate personnel. Provided technical assistance to other information systems staff, and I was responsible for systems recovery. I Insured that technical documentation and user manuals are accurate and current.  Provided customer support to staff of the Disability Determination Services (DDS). This included but is not limited to servicing computers, printers, network devices, and performing backup/restores requests. Installed printers, computers, Windows 7 imaging, and troubleshooting issues. Microsoft Share Point support. Scheduled repairs and working with service personnel, help desk duties. Telephone support to staffs for password issues. Weekend support and traveled to Louisville office when necessary. I have Experience with AS/400."
-        }
+        }		
     ]
 }
-            "description": "Installing and configuring computer hardware operating systems and applications.  Monitoring and maintaining computer systems and networks;  Talking staff/clients through a series of actions, either face to face or over the telephone; Troubleshooting system and network problems and diagnosing and solving hardware/software faults;  Replacing parts as required;  Wrote complex routines for system critical procedure and Diagnostic Routines  Following diagrams and written instructions to repair a fault or set up a system;  Supporting the roll-out of new applications;  Setting up new accounts and profiles and dealing with password issues;  Responding within agreed time limits to call-outs;  Working continuously on a task until completion (or referral to third parties, if appropriate);  Prioritizing and managing many open cases at one time; Rapidly establishing a good working relationship with customers and other professionals (e.g., software developers);  Testing and evaluating new technology;  Conducting electrical safety checks on computer equipment  used Dame ware to remote into clients pc’s from around the state to resolve a wide array of issues.  Install and maintain programs that are vital to everyday functions.  Work with third party companies to resolve issues with dell switches, as well as running Ethernet cable to connect Ports to the domain.  Used Active Directory to manage and maintain all devices on the domain"
-        }
-		{
-			"employer": "Kelly Services / Apple",
-			"title": "Senior Advisor",
-			"date": "05/27/2013 - 10/20/2013"
-			"description":"As a Senior Technical Advisor with apple I take ownership of the extremely difficult and technical cases that cannot be handled by our Tier 1 technical group, As well as technical cases I also receive Supervisor request that come from our customers. I build a rapport with the customer and troubleshoot their issue by using different steps I feel necessary to solve the   issue in a timely manner. I create repairs for devices that cannot be fixed by the usual troubleshooting steps or that have physical damage. If I feel a customer has been wronged or has not received the type of treatment that they should have I offer an appeasement in the manner of a device such as an IPod Touch. Being a senior technical advisor I work with Security Issues, Refunds, IPhones, IPods, Ipads, MacBook’s, Airport, and ITunes (In all its forms)."
-		}
-		{
-			"employer": "Adecco/Disability Determination Services",
-			"title": "IT Staff Assistant",
-			"date": "06/23/2011 - 08/16/2012",
-			"description": "Wrote complex routines for System Procedures and diagnostic routines. Installed and maintained hardware and software such as operating systems, control systems, workstations, peripherals and communications devices, proprietary software packages, information dissemination tools, electronic mail and data base management software. Performed complex system generations, network device configuration generations, workstation installation, network address definitions. Assisted the development, network and production support staff in analyzing requirements for development, modification or retirement of state level or complex departmental systems. I was responsible for system support functions during relocation or conversion of critical data files and equipment. Monitored performance of servers on local area networks and takes action to mitigate network or system degradation. Coordinated the diagnostics and problem resolution with network support staff. Received complex problem reports, and resolves or refers to appropriate personnel. Provided technical assistance to other information systems staff, and I was responsible for systems recovery. I Insured that technical documentation and user manuals are accurate and current.  Provided customer support to staff of the Disability Determination Services (DDS). This included but is not limited to servicing computers, printers, network devices, and performing backup/restores requests. Installed printers, computers, Windows 7 imaging, and troubleshooting issues. Microsoft Share Point support. Scheduled repairs and working with service personnel, help desk duties. Telephone support to staffs for password issues. Weekend support and traveled to Louisville office when necessary. I have Experience with AS/400."
-		}
-    ]
-}
-bio[education] ={
+education ={
     "Schools": [
         {
             "name": "Anderson County High School",
@@ -91,5 +70,38 @@ bio[education] ={
             }
         ]
     ]
-var formattedSkill = HTMLskills.replace("%data%", bio.skills );
-$('#header').append(bio.skill);
+}
+// -- Lines Below are appending my name and role to my resume --
+
+var name = "Immanuel Hensley";
+var role = "Front-End Web Developer";
+var formattedName = HTMLheaderName.replace("%data%", name);
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+
+//  -- Lines Below are the Skills sections appended to my resume --
+
+if (bio.skills.length != 0) {
+$('#header').append(HTMLskillsStart);
+var formattedSkill = HTMLskills.replace('%data%', bio.skills[0]);
+$('#skills').append(formattedSkill);
+formattedSkill = HTMLskills.replace('%data%', bio.skills[1]);
+$('#skills').append(formattedSkill);
+formattedSkill = HTMLskills.replace('%data%', bio.skills[2]);
+$('#skills').append(formattedSkill);
+}
+
+// -- Lines below are my jobs appended to my resume --
+ 
+for (job in work.jobs) {
+$('#workExperience').append(HTMLworkStart);
+var formattedEmployer = HTMLworkEmployer.replace('%data%', work.jobs[job].employer);
+var formattedTitle = HTMLworkTitle.replace('%data%', work.jobs[job].title);
+var formattedEmployerTitle = formattedEmployer + formattedTitle;
+var formattedWorkDates = HTMLworkDates.replace('%data%', work.jobs[job].date);
+var formattedWorkDescritption = HTMLworkDescription.replace('%data%',work.jobs[job].description);
+$('.work-entry:last').append(formattedEmployerTitle);
+$('.work-entry:last').append(formattedWorkDates);
+$('.work-entry:last').append(formattedWorkDescritption);
+}
