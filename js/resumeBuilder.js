a1e635c669f45e25c080f3f95b6684b3ca5517cc
Immanuel Hensley <hensleyimmanuel@yahoo.com>
2015-02-22 21:23:44 -0500

    got all locations to show up on map

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -45,14 +45,14 @@ work = {
             "employer": "Kelly Services / Apple",
             "title": "Senior Advisor",
             "date": "05/27/2013 - 10/20/2013",
-            "location": " Frankfort KY 40601",
+            "location": " 535 Shenandoah Dr. Frankfort KY 40601",
             "description": "As a Senior Technical Advisor with apple I take ownership of the extremely difficult and technical cases that cannot be handled by our Tier 1 technical group, As well as technical cases I also receive Supervisor request that come from our customers. I build a rapport with the customer and troubleshoot their issue by using different steps I feel necessary to solve the   issue in a timely manner. I create repairs for devices that cannot be fixed by the usual troubleshooting steps or that have physical damage. If I feel a customer has been wronged or has not received the type of treatment that they should have I offer an appeasement in the manner of a device such as an IPod Touch. Being a senior technical advisor I work with Security Issues, Refunds, IPhones, IPods, Ipads, MacBook’s, Airport, and ITunes (In all its forms)."
         },
         {
             "employer": "Adecco/Disability Determination Services",
             "title": "IT Staff Assistant",
             "date": "06/23/2011 - 08/16/2012",
-            "location": "Frankfort KY 40601",
+            "location": "934 Louisville Rd, Ste B Frankfort KY 40601",
             "description": "Wrote complex routines for System Procedures and diagnostic routines. Installed and maintained hardware and software such as operating systems, control systems, workstations, peripherals and communications devices, proprietary software packages, information dissemination tools, electronic mail and data base management software. Performed complex system generations, network device configuration generations, workstation installation, network address definitions. Assisted the development, network and production support staff in analyzing requirements for development, modification or retirement of state level or complex departmental systems. I was responsible for system support functions during relocation or conversion of critical data files and equipment. Monitored performance of servers on local area networks and takes action to mitigate network or system degradation. Coordinated the diagnostics and problem resolution with network support staff. Received complex problem reports, and resolves or refers to appropriate personnel. Provided technical assistance to other information systems staff, and I was responsible for systems recovery. I Insured that technical documentation and user manuals are accurate and current.  Provided customer support to staff of the Disability Determination Services (DDS). This included but is not limited to servicing computers, printers, network devices, and performing backup/restores requests. Installed printers, computers, Windows 7 imaging, and troubleshooting issues. Microsoft Share Point support. Scheduled repairs and working with service personnel, help desk duties. Telephone support to staffs for password issues. Weekend support and traveled to Louisville office when necessary. I have Experience with AS/400."
         }		
     ]
@@ -68,30 +68,32 @@ education ={
                 "IT"
             ],
             "url": "http://http://www.anderson.kyschools.us/andersoncountyhighschool_home.aspx"
-        },
-        "onlineCourses",
-        [
-            {
-                "Title": "Intro to HTML and CSS",
-                "School": "Udacity",
-                "Dates": "10/12/2014 - 12/8/2014",
-                "url": "https://www.udacity.com/course/viewer#!/c-ud304-nd/l-3342528615/m-3343838767"
-            },
-            {
-                "title": "How to use Gut and GitHub",
-                "School": "Udacity",
-                "dates": "12/8/2014 - 12/20/2014",
-                "url": "https://www.udacity.com/course/viewer#!/c-ud775-nd/l-3342528615/m-3343838767"
-            },
-            {
-                "title": "JavaScript Basics",
-                "schools": "Udacity",
-                "dates": "01/05/2015 - Present",
-                "url": "https://www.udacity.com/course/viewer#!/c-ud804-nd/l-1930528550"
-            }
-        ]
-    ]
-}
+        }
+		],
+        
+            "onlineCourses": [
+                {
+                    "Title": "Intro to HTML and CSS",
+                    "School": "Udacity",
+                    "Dates": "10/12/2014 - 12/8/2014",
+                    "url": "https://www.udacity.com/course/viewer#!/c-ud304-nd/l-3342528615/m-3343838767"
+                },
+                {
+                    "title": "How to use Gut and GitHub",
+                    "School": "Udacity",
+                    "dates": "12/8/2014 - 12/20/2014",
+                    "url": "https://www.udacity.com/course/viewer#!/c-ud775-nd/l-3342528615/m-3343838767"
+                },
+                {
+                    "title": "JavaScript Basics",
+                    "schools": "Udacity",
+                    "dates": "01/05/2015 - Present",
+                    "url": "https://www.udacity.com/course/viewer#!/c-ud804-nd/l-1930528550"
+                }
+            ]
+        }
+    
+
 // -- Lines Below are appending my name and role to my resume --
 
 function header() {
